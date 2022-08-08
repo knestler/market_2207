ce453ea335c3880991507c15bb7c6fb94b92457e
Krisetn Nestler <kristen.nestler@gmail.com>
2022-08-08 11:08:38 -0400

    I1: add item tests

---
+++ spec/item_spec.rb
@@ -0,0 +1,20 @@
+require './lib/item'
+
+RSpec.describe Item do
+
+  before :each do
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato',price: '$0.50'})
+  end
+
+  it 'exists' do 
+    expect(@item1).to be_instance_of(Item)
+    expect(@item2).to be_instance_of(Item)
+  end 
+
+  it "has attributes" do
+    expect(@item2.name).to eq("Tomato")
+    expect(@item2.price).to eq(0.5)
+  end 
+
+end
